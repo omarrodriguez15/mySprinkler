f0c8f4a7a7373168faccc1a37b5c99f8d164bf47
omarr2012 <omarrodriguez@Omar-Rodriguezs-MacBook-Pro.local>
2015-09-20 14:22:09 -0500

    Load routes, models, and controllers. Setup weather requesting.
    Right now manually writing weather request body returned from open
    weather to weather collection. The latitude and longitude are hardcoded.

--- omarsSandbox/server/index.js
+++ omarsSandbox/server/index.js
@@ -2,6 +2,13 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+//lat = 30.627977 & lon = -96.334407
+var weather = require('request').defaults({
+  url: 'http://api.openweathermap.org/data/2.5/weather',
+  json: true
+});
+
+
 var _ = require('lodash');
 
 //Create the application
@@ -24,20 +31,30 @@ app.use('/hello',function(req, res, next){
 	res.send('Hello mySprinkler');
 	next();
 });
-
+app.use('/we',function(req, res, next){
+	var latT = '30.627977', lonT = '-96.334407';
+	//append the qs(query string of latitude and longitude)
+	weather.get({ qs: { lat: latT, lon: lonT} }, function (error, weatherResponse, body) {
+		if (error) return console.log(error);
+		db.collection('weather').insert(body);
+		//JSON.stringify(body);
+		console.log(body);
+	});
+	next();
+});
 
 //Connect to MongoDB
 mongoose.connect('mongodb://localhost/mySprinkler');
-mongoose.connection.once('open',function(){
+var db = mongoose.connection.once('open',function(){
 
 	//Load the models
-	//app.models = require('./models/index');
+	app.models = require('./models/index');
 	
 	//Load the routes
-	/*var routes = require('./routes');
+	var routes = require('./routes');
 	_.each(routes, function(controller, route){
 		app.use(route, controller(app, route));
-	});*/
+	});
 	
 	console.log('listening on port 3000...');
 	app.listen(3000);
