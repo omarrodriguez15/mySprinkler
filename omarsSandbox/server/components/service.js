8c4db17bbcd597d123b5437c19c847e404c3195b
omarr2012 <omarrodriguez@dhcp-10-202-246-53.tamulink.tamu.edu>
2015-10-26 10:06:47 -0500

    setup templates for calls to server
    very rough starts to the calls to the web server from pi, but they are
    heavily commented.

--- omarsSandbox/server/components/service.js
+++ omarsSandbox/server/components/service.js
@@ -6,37 +6,34 @@ var scheduleSchema = require('../models/schedule');
 var schedule = mongoose.model('schedule', scheduleSchema);
 var http = require('http');
 
+//For testing its default hostname is localhost
+//production this will be set in config
+var options = {
+		port: config.options.port,
+	};
+
 module.exports = {
 	beginTimer : function(){
 		//3600000 miliseconds is an hour
 		setInterval(postSchedule, 5000);
 	},
 	sendIp : function(){
-		return 'send Ip';
+		return console.log('send Ip');
 	},
 	getWeather: function(){
-		return 'get weather';
+		return console.log('get weather');
 	},
 	sendSn : function(){
-		return 'send serial number';
+		return console.log('send serial number');
 	} 
 };
 
-function postIp(){
-	
-}
-
-function getWeather(){
-	
-}
-
-function postSchedule(){
-	var options = {
-	//defaulted to hostnam at port 80
-	port: config.options.port || 9000,
-	path: '/api/schedules',
-	method: 'GET'
-	};
+//Needs to get current IP address and PUT it to
+//the web server pi collection by using its object ID to find
+//the correct document
+function putIp(){
+	options.path = '/api/pi';
+	options.method = 'PUT';
 	
 	http.request(options, function(res){
 		
@@ -50,5 +47,69 @@ function postSchedule(){
 		});
 		
 	}).end();
-	console.log('Send Info Fired!');
+	console.log('PUT Pi info Fired!');
+}
+
+//If first time need to create document which includes serial number of pi
+//which is in the config file and a status of 0=notverified 
+function postIp(){
+	options.path = '/api/pi';
+	options.method = 'POST';
+	
+	http.request(options, function(res){
+		
+		res.on('data', function(d) {
+			var data = JSON.parse(d);
+			console.log(data);
+		});
+		
+		res.on('error', function(e) {
+			console.log('problem with request: ' + e.message);
+		});
+		
+	}).end();
+	console.log('Post Ip Fired!');
+}
+
+//need to grab most current weather from web server
+//then decide what to write to local db depending on when the
+//last time you successfully communincated with the webserver
+function getWeather(){
+	options.path = '/api/weather';
+	options.method = 'GET';
+	
+	http.request(options, function(res){
+		
+		res.on('data', function(d) {
+			var data = JSON.parse(d);
+			console.log(data);
+		});
+		
+		res.on('error', function(e) {
+			console.log('problem with request: ' + e.message);
+		});
+		
+	}).end();
+	console.log('Get Weather Fired!');
+}
+
+//For testing its sending a get request not post
+//Production should 
+function postSchedule(){
+	options.path = '/api/schedules';
+	options.method = 'GET';
+	
+	http.request(options, function(res){
+		
+		res.on('data', function(d) {
+			var data = JSON.parse(d);
+			console.log(data);
+		});
+		
+		res.on('error', function(e) {
+			console.log('problem with request: ' + e.message);
+		});
+		
+	}).end();
+	console.log('GET Schedules Fired!');
 }
