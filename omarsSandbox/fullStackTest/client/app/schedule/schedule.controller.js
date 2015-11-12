e038f7f5e53ff6031df185f0aaefdbd461e24fa8
omarr2012 <omarrodriguez@omarrodriguezs-MacBook-Pro.local>
2015-11-11 20:34:29 -0600

    Schedules now pulling from api
    also changed authentication check

--- omarsSandbox/fullStackTest/client/app/schedule/schedule.controller.js
+++ omarsSandbox/fullStackTest/client/app/schedule/schedule.controller.js
@@ -1,29 +1,43 @@
 'use strict';
-var Tweek, Ttimes;
+var week, times;
 angular.module('fullStackTestApp')
   .controller('ScheduleCtrl', function ($scope, $location, $cookieStore, $http, Auth) {
-    if(!$cookieStore.get('token')) {
+    //check if user is logged in
+    if(!Auth.isLoggedIn()) {
       $location.path('/login');
     }
+    //Grab user info stored in cookie
     var user = Auth.getCurrentUser();
-    console.log(user.schedId);
-    $http.get('/api/schedules/'+user.schedId).success(function(res) {
-        if (res.length < 1) {
-          return console.log('no schedule found!');
-        }
-        
-        $scope.schedule = res;
-        console.log(res);
-      });
+    console.log(user);
     
-    $scope.times = Ttimes;
-    $scope.week = Tweek;
+    $http.get('/api/schedules/'+user.schedId).success(function(res) {
+      if (res.length < 1) {
+        return console.log('no schedule found!');
+      }
+      console.log(res);
+      
+      createScheduleArray(res, function(newWeek){
+        $scope.times = times;
+        $scope.week = newWeek;
+      });
+      
+    });
   });
 
+function createScheduleArray(sched, cb){
+  var newWeek = week;
+  for(var i = 0; i < week.length ; i++){
+    var day = week[0].day.toString().toLowerCase();
+    
+    newWeek[i].start = sched[day].start;
+    newWeek[i].end = sched[day].end;
+  }
+  //send back new week object
+  cb(newWeek);
+}
 
 
-//Dummy Data
-Tweek = [
+week = [
       {
         day:'Monday'
       },
@@ -46,6 +60,6 @@ Tweek = [
         day:'Sunday'
       }
       ];
-Ttimes = ['12:00 AM','01:00 AM','02:00 AM','03:00 AM','04:00 AM','05:00 AM','06:00 AM','07:00 AM','08:00 AM','09:00 AM',
+times = ['12:00 AM','01:00 AM','02:00 AM','03:00 AM','04:00 AM','05:00 AM','06:00 AM','07:00 AM','08:00 AM','09:00 AM',
         '10:00 AM','11:00 AM','12:00 PM','01:00 PM','02:00 PM','03:00 PM','04:00 PM','05:00 PM','06:00 PM','07:00 PM',
         '08:00 PM','09:00 PM','10:00 PM', '11:00 PM'];
