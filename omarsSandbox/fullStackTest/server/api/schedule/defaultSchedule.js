4e0ec08f30214b22ddd8b450247bda1bda3d929e
Heriberto Rodriguez <hrodz13@tamu.edu>
2015-12-02 20:29:11 -0600

    Fixed Schedule

--- omarsSandbox/fullStackTest/server/api/schedule/defaultSchedule.js
+++ omarsSandbox/fullStackTest/server/api/schedule/defaultSchedule.js
@@ -1,37 +1,37 @@
 module.exports = {
   monday:{
-    start: '7:00',
-    end: '8:00',
+    start: '07:00',
+    end: '08:00',
     status: '0'
   },
   tuesday:{
-    start: '7:00',
-    end: '8:00',
+    start: '07:00',
+    end: '08:00',
     status: '0'
   },
   wednesday:{
-    start: '7:00',
-    end: '8:00',
+    start: '07:00',
+    end: '08:00',
     status: '0'
   },
   thursday:{
-    start: '7:00',
-    end: '8:00',
+    start: '07:00',
+    end: '08:00',
     status: '0'
   },
   friday:{
-    start: '7:00',
-    end: '8:00',
+    start: '07:00',
+    end: '08:00',
     status: '0'
   },
   saturday:{
-    start: '7:00',
-    end: '8:00',
+    start: '07:00',
+    end: '08:00',
     status: '0'
   },
   sunday:{
-    start: '7:00',
-    end: '8:00',
+    start: '07:00',
+    end: '08:00',
     status: '0'
   },
 };
